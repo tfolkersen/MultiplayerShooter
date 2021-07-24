31b1ba02a8d28d2840932342e43d8486519e0852
Taylor Folkersen <folkerse@ualberta.ca>
2021-07-24 10:09:05 -0600

    death animation shader

---
+++ ShaderOverlay.gd
@@ -0,0 +1,46 @@
+extends ColorRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fillScreen()
+	
+	
+var playing = false
+var frame = 0
+var lastFrame = 280
+var black = false
+
+func _process(delta):
+	if playing:
+		get_material().set_shader_param("frame", frame)
+		get_material().set_shader_param("lastFrame", lastFrame)
+		visible = true
+	else:
+		visible = false
+		
+	if Input.is_key_pressed(KEY_KP_0):
+		startAnimation()
+	if Input.is_key_pressed(KEY_KP_1):
+		playing = false
+		
+	frame += 1
+	
+	
+func startAnimation():
+	playing = true
+	frame = 0
+	
+func fillScreen():
+	var size = get_viewport().size
+	rect_size = size
+	get_material().set_shader_param("dims", size)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
