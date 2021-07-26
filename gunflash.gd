7d42e6e57daec33ec9d3a01d36d824577166f9e3
Taylor Folkersen <folkerse@ualberta.ca>
2021-07-26 05:50:09 -0600

    gun flash progress

--- gunflash.gd
+++ gunflash.gd
@@ -8,7 +8,9 @@ func _ready():
 	timer.connect("timeout", self, "timedOut")
 
 func show():
+	print("Showing flash")
 	visible = true
+	rotation_degrees = Vector3(randf() * 360.0, randf() * 360.0, randf() * 360.0)
 	timer.stop()
 	timer.start(0.1)
 	
