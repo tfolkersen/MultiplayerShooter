86acd29bbe0e36ffc821f79221aee2cbbc8ba9e5
Taylor Folkersen <folkerse@ualberta.ca>
2021-07-28 23:59:32 -0600

    icon and folder structure

---
+++ Scripts/Lobby.gd
@@ -0,0 +1,138 @@
+extends Control
+
+var messageScene = preload("res://Scenes/LobbyMessage.tscn")
+
+onready var xd = randi()
+
+remotesync func setReady():
+	var id = get_tree().get_rpc_sender_id()
+	var node = get_node("PlayerList/VBoxContainer/" + str(id))
+	node.setText("ready")
+	node.setSenderColor(Color(0.0, 1.0, 0.0))
+	
+remotesync func setNotReady():
+	var id = get_tree().get_rpc_sender_id()
+	var node = get_node("PlayerList/VBoxContainer/" + str(id))
+	node.setText("not ready")
+	node.setSenderColor(Color(1.0, 0.0, 0.0))
+
+func addUserToList(name, id):
+	var message = messageScene.instance()
+	message.setContent(name, "not ready")
+	message.setSenderColor(Color(1.0, 0.0, 0.0))
+	message.name = str(id)
+	$PlayerList/VBoxContainer.add_child(message)
+
+func removeUserFromList(id):
+	var node = get_node("PlayerList/VBoxContainer/" + str(id))
+	if node:
+		node.queue_free()
+
+func testChat():
+	var users = ["memer", "xxQuickscopes420xx", "asdasdasdasd"]
+	var words = ["nice", "meme", "haha", "xd"]
+	
+	randomize()
+	for i in range(50):
+		var u = users[randi() % len(users)]
+		var m = ""
+		for w in range(randi() % 10 + 1):
+			m += words[randi() % len(words)] + " "
+		
+		addMessage(u, m)
+
+func testUserList():
+	var users = ["memer", "xxQuickscopes420xx", "asdasdasdasd"]
+	var words = ["nice", "meme", "haha", "xd"]
+	
+	randomize()
+	for i in range(10):
+		var u = users[randi() % len(users)]
+		addUserToList(u, i)
+
+func _ready():
+	$MessageEdit.grab_focus()
+	$MessageEdit.text = "/start"
+	
+	print("Lobby Joined tree")
+	#testChat()
+	#testUserList()
+	var dims = get_viewport().size
+	rect_size = dims
+	
+	var prev = null
+	var curr = null
+	
+	curr = $ChatWindow
+	curr.rect_size = Vector2(dims.x * 0.65, dims.y * 0.9)
+	curr.rect_position = Vector2(dims.x - curr.rect_size.x, 0)
+	
+	prev = curr
+	curr = $MessageEdit
+	curr.rect_size = Vector2(prev.rect_size.x, dims.y - prev.rect_size.y)
+	curr.rect_position = Vector2(prev.rect_position.x, prev.rect_size.y)
+	
+	prev = curr
+	curr = $PlayerList
+	curr.rect_position = Vector2(0, 0)
+	curr.rect_size = Vector2(dims.x - $ChatWindow.rect_size.x, dims.y / 2.0)
+
+func addMessage(sender, text, senderColor = Color(1.0, 1.0, 0.0)):
+	var message = messageScene.instance()
+	message.setContent(sender, text)
+	message.setSenderColor(senderColor)
+	$ChatWindow/VBoxContainer.add_child(message)
+	
+remotesync func receiveMessage(message):
+	var id = get_tree().get_rpc_sender_id()
+	if not Network.peers.has(id):
+		return
+	var peer = Network.peers[id]
+	addMessage(peer.name, message)
+	
+remotesync func systemMessage(message):
+	addMessage("[SYSTEM]", message, Color(1.0, 0.0, 1.0))
+
+func quitLobby():
+	Network.disconnectNetwork()
+	queue_free()
+	Global.showMainMenu()
+
+func _messageEntered(text):
+	$MessageEdit.clear()
+	if text == "":
+		return
+		
+	if text == "/help":
+		var helpString = "Commands:\n/help -- show this message\n/start or /startgame -- start game if host\n/disconnect or /quit -- leave lobby\n/ready -- mark self as ready\n/unready or /notready -- mark self as not ready\n/host -- show who is host"
+		systemMessage(helpString)
+		return
+	if text in ["/start", "/startgame"] and is_network_master():
+		Network.rpc("startGame")
+		return
+	if text in ["/disconnect", "/quit"]:
+		quitLobby()
+		return
+	if text == "/ready":
+		rpc("setReady")
+		if not is_network_master():
+			setReady()
+		return
+	if text in ["/unready", "/notready"]:
+		rpc("setNotReady")
+		if not is_network_master():
+			setNotReady()
+		return
+	if text == "/host":
+		systemMessage("Host is \"" + str(Network.peers[1].name) + "\"")
+		return
+
+	rpc("receiveMessage", text)
+	if not is_network_master():
+		receiveMessage(text)
+
+func _process(delta):
+	pass
+
+func _exitingTree():
+	print("Lobby exiting tree")
---
+++ Scripts/Network.gd
@@ -0,0 +1,158 @@
+extends Node
+
+var lobbyInstance = null
+var gameInstance = null
+
+var networkID = 0
+var peers = {}
+
+remotesync func addPeer(name):
+	var id = get_tree().get_rpc_sender_id()
+	print("Adding peer [" + str(id) + "]" + name)
+	if peers.has(id):
+		return
+	peers[id] = {"id": id, "name": name}
+	if id != networkID:
+		rpc_id(id, "addPeer", Global.playerName)
+	lobbyInstance.addUserToList(name, id)
+	if lobbyInstance.is_network_master():
+		lobbyInstance.rpc("systemMessage", name + " has joined")
+
+func removePeer(id):
+	print("Removing peer" + str(id))
+	if peers.has(id):
+		var peer = peers[id]
+		if is_instance_valid(lobbyInstance):
+			lobbyInstance.systemMessage(peer.name + " has left")
+			lobbyInstance.removeUserFromList(id)
+			
+		if is_instance_valid(gameInstance):
+			var playerNode = gameInstance.get_node("players/" + str(id))
+			playerNode.queue_free()
+
+func _ready():
+	print("Network ready")
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+
+func _player_connected(id):
+	# Called on both clients and server when a peer connects. Send my info to it.
+	print("Player connected " + str(id))
+	rpc_id(id, "addPeer", Global.playerName)
+
+func _player_disconnected(id):
+	print("Player disconnected " + str(id))
+	removePeer(id)
+	
+func _connected_ok():
+	print("Connected to server")
+	rpc("addPeer", Global.playerName)
+	lobbyInstance.visible = true
+	Global.closeMainMenu()
+
+func _server_disconnected():
+	print("Disconnected by server")
+	if is_instance_valid(lobbyInstance):
+		stopGame()
+		lobbyInstance.quitLobby()
+		lobbyInstance = null
+	Network.disconnectNetwork()
+
+func _connected_fail():
+	print("Failed to connect to server")
+	pass # Could not even connect to server; abort.
+
+func createServer(port):
+	peers = {}
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(int(port))
+	get_tree().network_peer = peer
+	networkID = peer.get_unique_id()
+
+	print("Created server. I am ID " + str(networkID))
+	
+func createClient(ip, port):
+	peers = {}
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(ip, int(port))
+	get_tree().network_peer = peer
+	networkID = peer.get_unique_id()
+	print("Created client. I am ID " + str(networkID))
+	
+func disconnectNetwork():
+	var peer = get_tree().network_peer
+	if is_instance_valid(peer):
+		peer.close_connection()
+		peer = null
+		networkID = 0
+		print("Closed network connection")
+		peers = {}
+
+func hostLobby(port):
+	print("Hosting lobby")
+	if is_instance_valid(lobbyInstance):
+		lobbyInstance.quitLobby()
+		lobbyInstance = null
+	var lobbyScene = preload("res://Scenes/Lobby.tscn")
+	lobbyInstance = lobbyScene.instance()
+	get_node("/root/Game").add_child(lobbyInstance)
+	
+	createServer(port)
+	rpc("addPeer", Global.playerName)
+	
+	Global.closeMainMenu()
+
+var playerScene = preload("res://Scenes/Player.tscn")
+
+remotesync func startGame():
+	if is_instance_valid(gameInstance):
+		stopGame()
+		
+	#Make level
+	lobbyInstance.visible = false
+	#var levelScene = preload("res://Maps/TestMap.tscn")
+	var levelScene = preload("res://Maps/TempleMap.tscn")
+	gameInstance = levelScene.instance()
+	gameInstance.name = "Level"
+	get_node("/root/Game").add_child(gameInstance)
+	
+	#Make players
+	var players = Node.new()
+	players.set_name("players")
+	gameInstance.add_child(players)
+	
+	for id in peers:
+		var player = playerScene.instance()
+		print("Making player with ID " + str(id))
+		player.global_transform.origin = Vector3(0, 2, 0) + Vector3(0, 3.5 * ((id - 1) % 2), 0)
+		player.set_name(str(id))
+		player.set_network_master(id)
+		players.add_child(player)
+		#gameInstance.add_child(player)
+		
+	Global.captureMouse()
+
+remotesync func stopGame():
+	if is_instance_valid(gameInstance):
+		gameInstance.queue_free()
+		gameInstance = null
+		Global.releaseMouse()
+
+func joinLobby(ip, port):
+	print("Joining lobby")
+	if is_instance_valid(lobbyInstance):
+		lobbyInstance.quitLobby()
+		lobbyInstance = null
+	
+	var lobbyScene = preload("res://Scenes/Lobby.tscn")
+	lobbyInstance = lobbyScene.instance()
+	lobbyInstance.visible = false
+	get_node("/root/Game").add_child(lobbyInstance)
+	
+	#Global.closeMainMenu()
+	
+	createClient(ip, port)
+	#addPeer(Global.playerName)
