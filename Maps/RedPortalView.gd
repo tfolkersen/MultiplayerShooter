59acef10fb5a7bad5f6166bc5a4fe27d0d5ccfa4
Taylor Folkersen <folkerse@ualberta.ca>
2021-07-31 11:01:39 -0600

    portal stuff

--- Maps/RedPortalView.gd
+++ Maps/RedPortalView.gd
@@ -1,18 +1,23 @@
 extends Sprite3D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+
 
 
 func _process(delta):
-	var tex = get_viewport().get_texture()
+	var frac = (sin(time / 2.0) + 1.0) / 2.0
+	$OmniLight9.light_color = Color(1.0, 0.0, 0.0, 1.0).linear_interpolate(Color(1.0, 1.0, 0.0, 1.0), frac)
+	time += delta;
+	
 	material_override.set_shader_param("viewportData", texture)
 	material_override.set_shader_param("color1", Vector3(1.0, 0.0, 0.0))
 	material_override.set_shader_param("color2", Vector3(1.0, 1.0, 0.0))
